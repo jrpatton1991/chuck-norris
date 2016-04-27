448fec6497a1c229b6d8e7ab620a3bcc7b9cf469
Skyn3t <lzito406@gmail.com>
2016-04-27 11:03:13 -0500

    Initial connection for CN API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Chuck Norris' });
 });
+
+router.get('/newRandom', function(req, res, next){
+  request('http://api.icndb.com/jokes/random?callback=', function(error, response, body) {
+   if (!error && response.statusCode == 200) {
+     body();
+     console.log(body)
+     res.json(body);
+   };
+ });
+});
+
+
+
+
 
 module.exports = router;
